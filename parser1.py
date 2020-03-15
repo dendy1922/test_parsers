fd12befc371704a4ecd645fa130dfa40c91ccb44
Tim <dragun1921@gmail.com>
2020-03-16 00:24:31 +0300

    Second

--- parser1.py
+++ parser1.py
@@ -1,17 +1,35 @@
-import requests
-import lxml
 import json
 import logging
+import time
+import lxml
+import requests
 
 from bs4 import BeautifulSoup
 
+
+URL  = "https://www.mebelshara.ru/contacts"
+
+
+def main():
+    """
+    Main function that starting all actions
+    """
+
+    logging.basicConfig(format="%(asctime)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S",
+                        filename="parser1.log", level=logging.INFO, filemode="w")
+
+    logging.info("Starting")
+    data = get_data(get_html(URL))
+    logging.info("Got data from the server.Put data to the list")
+    to_json(data)
+    logging.info("Put to json file. Done!")
 
 
 def to_json(data):
     """
     At the begging clean json file after putting data to json file.
     """
-    with open('parser1.json', 'w') as f:
+    with open("parser1.json", "w") as f:
         json.dump(data, f)
 
 
@@ -21,30 +39,32 @@ def get_data(html):
     """
     spis = []
 
-    soup = BeautifulSoup(html, 'lxml')
-    phones = soup.find('span',class_="phone-num zphone").text
-    cities = soup.find_all('div', class_ ="city-item")
+    soup = BeautifulSoup(html, "lxml")
+    phones = soup.find("span", class_="phone-num zphone").text
+    cities = soup.find_all("div", class_ ="city-item")
 
     for city in cities:
 
-        city_name = city.find('h4').text
-        places = city.find_all('div', class_="shop-list-item")
+        city_name = city.find("h4").text
+        places = city.find_all("div", class_="shop-list-item")
 
         for place in places:
 
-            local_address = "{}, {}".format(city_name,place.find('div', class_ = "shop-address").text)
-            latlon = [place['data-shop-latitude'], place['data-shop-longitude']]
-            name = place['data-shop-name']
-            working_hours = "{} {}".format(place['data-shop-mode1'],place['data-shop-mode2'])
+            local_address = "{}, {}".format(city_name, place.find("div", class_ = "shop-address").text)
+            latlon = [place["data-shop-latitude"], place["data-shop-longitude"]]
+            name = place["data-shop-name"]
+            working_hours = "{} {}".format(place["data-shop-mode1"], place["data-shop-mode2"])
 
-            data = {'address': local_address,
-                    'latlon': latlon,
-                    'name': name,
-                    'phones': phones,
-                    'working_hours':working_hours
+            data = {
+                    "address": local_address,
+                    "latlon": latlon,
+                    "name": name,
+                    "phones": phones,
+                    "working_hours":working_hours,
                     }
 
             spis.append(data)
+    return spis
 
 
 def get_html(url):
@@ -55,23 +75,6 @@ def get_html(url):
     if resp.ok:
         return resp.text
     logging.warning("Connection problems")
-
-
-def main():
-    """
-    Main function that starting all actions
-    """
-
-    logging.basicConfig(format='%(asctime)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S',
-                        filename="parser1.log", level=logging.INFO,filemode='w')
-
-    url = 'https://www.mebelshara.ru/contacts'
-
-    logging.info("Starting")
-    data = get_data(get_html(url))
-    logging.info("Got data from the server.Put data to the list")
-    to_json(data)
-    logging.info("Put to json file. Done!")
 
 
 if __name__ == "__main__":
--- parser2.py
+++ parser2.py
@@ -1,10 +1,14 @@
-import requests
-import lxml
 import json
-import time 
 import logging
+import time
+import lxml
+import requests
 
 from bs4 import BeautifulSoup
+
+
+URL = "https://www.tui.ru/api/office/cities/"
+BASIC_URL = "https://www.tui.ru/api/office/list/?cityId="
 
 
 def main():
@@ -12,31 +16,27 @@ def main():
     Main function that starting all actions
     """
 
-    logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', 
-                        filename="parser2.log", level=logging.INFO, filemode='w')
-
-    url = 'https://www.tui.ru/api/office/cities/'
-    basic_url = 'https://www.tui.ru/api/office/list/?cityId='
+    logging.basicConfig(format="%(asctime)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S", 
+                        filename="parser2.log", level=logging.INFO, filemode="w")
 
     logging.info("Starting")
-    data = get_json(get_html(url))
+    data = json.loads(get_html(URL))
     logging.info("Got request to the server")
     list_cities = get_cities(data)
     logging.info("Got list of cities")
 
-    global json_list
     json_list = []
 
-    logging.info("Starting cicle for each city")
+    logging.info("Starting cycle for each city")
     for item in list_cities:
 
-        url_item = basic_url + str(item)
-        data = get_json(get_html(url_item))
-        get_data(data)
-        logging.info(f'Request to the: {url_item} url done successfully. Put data to the list. ')
+        url_item = BASIC_URL + str(item)
+        data = json.loads(get_html(url_item))
+        json_list = get_data(data, json_list)
+        logging.info(f"Request to the: {url_item} url done successfully. Put data to the list. ")
         time.sleep(0.2)
 
-    logging.info("End cicle. Got data from the server. Put data to the list")
+    logging.info("End cycle. Got data from the server. Put data to the list")
     to_json(json_list)
     logging.info("Put to json file. Done!")
 
@@ -46,8 +46,9 @@ def get_cities(data):
     Getting all cities and returnig list
     """
     spis_cit = []
+    
     for item in data:
-        spis_cit.append(item['cityId'])
+        spis_cit.append(item["cityId"])
     return spis_cit
 
 
@@ -55,57 +56,52 @@ def get_working_hours(data):
     """
     Function for formating working hours
     """
-    if not data.get('workdays').get('isDayOff'):
-        workdays = "пн-пт {} до {}".format(data.get('workdays').get('startStr'),data.get('workdays').get('endStr'))
+    if not data.get("workdays").get("isDayOff"):
+        workdays = "пн-пт {} до {}".format(data.get("workdays").get("startStr"), data.get("workdays").get("endStr"))
     else:
         workdays = "пн-пт выходной"
         
-    if not data.get('saturday').get('isDayOff'):
-        saturday = "сб {} до {}".format(data.get('saturday').get('startStr'),data.get('saturday').get('endStr'))
+    if not data.get("saturday").get("isDayOff"):
+        saturday = "сб {} до {}".format(data.get("saturday").get("startStr"), data.get("saturday").get("endStr"))
     else:
         saturday = "сб выходной"
 
-    if not data.get('sunday').get('isDayOff'):
-        sunday = "вс {} до {}".format(data.get('sunday').get('startStr'),data.get('sunday').get('endStr'))
+    if not data.get("sunday").get("isDayOff"):
+        sunday = "вс {} до {}".format(data.get("sunday").get("startStr"), data.get("sunday").get("endStr"))
     else:
         sunday = "вс выходной"
 
     return [workdays, saturday, sunday]
 
 
-def get_data(data):
+def get_data(data, json_list):
     """
     Getting data from the snippet and putting to the list
     """
     for item in data:
 
-        address = item.get('address')
+        address = item.get("address")
 
-        latlon = [item.get('latitude'), item.get('longitude')]
+        latlon = [item.get("latitude"), item.get("longitude")]
 
-        name = item.get('name')
+        name = item.get("name")
 
-        phones = [item['phone'] for item in item.get('phones', '')]
+        phones = [item["phone"] for item in item.get("phones", "")]
 
-        hours_dict = item.get('hoursOfOperation')
+        hours_dict = item.get("hoursOfOperation")
         working_hours = get_working_hours(hours_dict)
 
 
-        result_data = {'address': address,
-                       'latlon': latlon,
-                       'name': name,
-                       'phones': phones,
-                       'working_hours':working_hours
+        result_data = {
+                       "address": address,
+                       "latlon": latlon,
+                       "name": name,
+                       "phones": phones,
+                       "working_hours":working_hours,
                       }
         json_list.append(result_data)
 
-
-def get_json(json_data):
-    """
-    Loads data from json
-    """
-    load_data = json.loads(json_data)
-    return load_data
+    return json_list
 
 
 def get_html(url):
@@ -122,7 +118,7 @@ def to_json(data):
     """
     At the begging clean json file after putting data to json file.
     """
-    with open('parser2.json', 'w') as f:
+    with open("parser2.json", "w") as f:
         json.dump(data, f)
 
 
